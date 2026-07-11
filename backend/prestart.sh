3a467bfb6104e68f06155ceb1628e89157bfa42b
XztCloud <963966598@qq.com>
2026-07-11 20:23:34 +0800

    修改打包配置

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 # Let the DB start
-python app/prestart/prestart.py
+init-db
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python app/prestart/create_admin.py
+create-admin
